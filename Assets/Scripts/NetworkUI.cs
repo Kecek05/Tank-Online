af068d53f8b7b61f0dd067c0b89f6edc95c981c8
João Felipe <jfk.abrao@hotmail.com>
2025-01-23 09:58:43 -0300

    v2

---
+++ Assets/Scripts/NetworkUI.cs
@@ -0,0 +1,29 @@
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkUI : MonoBehaviour
+{
+    [SerializeField] private Button hostButton;
+    [SerializeField] private Button clientButton;
+
+
+    private void Awake()
+    {
+        hostButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartHost();
+            Hide();
+        });
+        clientButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartClient();
+            Hide();
+        });
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
