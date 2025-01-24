fe560e8ea8eec2a9ac04b1a1101c974423766c6e
João Felipe <jfk.abrao@hotmail.com>
2025-01-23 21:32:30 -0300

    v12
    17. Health Display

---
+++ Assets/Scripts/Core/Combat/HealthDisplay.cs
@@ -0,0 +1,30 @@
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : NetworkBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Health health;
+    [SerializeField] private Image healthBarImage;
+
+    public override void OnNetworkSpawn()
+    {
+        if(!IsClient) return;
+
+        health.currentHealth.OnValueChanged += HandleHealthChanged;
+        HandleHealthChanged(0, health.currentHealth.Value); // if changed before subscribing
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (!IsClient) return;
+
+        health.currentHealth.OnValueChanged -= HandleHealthChanged;
+    }
+
+    private void HandleHealthChanged(int oldHealth, int newHealth)
+    {
+        healthBarImage.fillAmount = (float)newHealth / health.maxHealth;
+    }
+}
