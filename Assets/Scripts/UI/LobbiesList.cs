24c39c87efa05707284cfc13a85b8e4b42d973d6
KeceK <jfk.abrao@hotmail.com>
2025-01-28 16:46:05 -0300

    v31
    Almos done joing lobby

---
+++ Assets/Scripts/UI/LobbiesList.cs
@@ -0,0 +1,89 @@
+using System.Collections.Generic;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbiesList : MonoBehaviour
+{
+    [SerializeField] private LobbyItem lobbyItemPrefab;
+    [SerializeField] private Transform lobbyItemParent;
+    [SerializeField] private Button refreshBtn;
+
+    private bool isJoining;
+    private bool isRefreshing;
+
+    private void Awake()
+    {
+        refreshBtn.onClick.AddListener(RefreshListAsync);
+    }
+
+    private void OnEnable()
+    {
+        RefreshListAsync();
+    }
+
+    public async void RefreshListAsync()
+    {
+        if (isRefreshing) return;
+
+        isRefreshing = true;
+
+        try
+        {
+            QueryLobbiesOptions queryLobbiesOptions = new QueryLobbiesOptions();
+            queryLobbiesOptions.Count = 25;
+
+            queryLobbiesOptions.Filters = new List<QueryFilter>()
+            {
+            new QueryFilter(field: QueryFilter.FieldOptions.AvailableSlots, op: QueryFilter.OpOptions.GT, value: "0"),
+            new QueryFilter(field: QueryFilter.FieldOptions.IsLocked, op: QueryFilter.OpOptions.EQ, value: "0") // 0 = false
+            };
+
+            QueryResponse lobbies = await LobbyService.Instance.QueryLobbiesAsync(queryLobbiesOptions);
+
+            // clear list
+            foreach (Transform child in lobbyItemParent)
+            {
+                Destroy(child.gameObject);
+            }
+
+            // add to  list
+            foreach(Lobby lobby in lobbies.Results)
+            {
+                LobbyItem lobbyItem = Instantiate(lobbyItemPrefab, lobbyItemParent);
+                lobbyItem.Initialise(this, lobby);
+            }
+
+        } catch (LobbyServiceException e)
+        {
+            Debug.LogError(e);
+        }
+
+        
+
+        isRefreshing = false;
+    }
+
+    public async void JoinAsync(Lobby lobby)
+    {
+        if (isJoining) return;
+
+        isJoining = true;
+        // Join the lobby
+        try
+        {
+            Lobby joinedLobby = await LobbyService.Instance.JoinLobbyByIdAsync(lobby.Id);
+            string joinCode = joinedLobby.Data["JoinCode"].Value;
+
+            await ClientSingleton.Instance.GameManager.StartClientAsync(joinCode);
+
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.LogError(e);
+        }
+
+        isJoining = false;
+    }
+}
---
+++ Assets/Scripts/UI/LobbyItem.cs
@@ -0,0 +1,36 @@
+using TMPro;
+using Unity.Services.Lobbies.Models;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyItem : MonoBehaviour
+{
+    [SerializeField] private TMP_Text lobbyNameTxt;
+    [SerializeField] private TMP_Text lobbyPlayersTxt;
+    [SerializeField] private Button joinBtn;
+
+    private LobbiesList lobbiesList;
+    private Lobby lobby;
+
+    private void Awake()
+    {
+        joinBtn.onClick.AddListener(() =>
+        {
+            // call to join the lobby
+            lobbiesList.JoinAsync(lobby);
+        });
+    }
+
+
+    public void Initialise(LobbiesList lobbiesList ,Lobby lobby)
+    {
+        this.lobbiesList = lobbiesList;
+        this.lobby = lobby;
+
+        lobbyNameTxt.text = lobby.Name;
+        lobbyPlayersTxt.text = $"{lobby.Players.Count}/{lobby.MaxPlayers}";
+    }
+
+
+
+}
