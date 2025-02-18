351e9fe4d32084d8b25ee7e9f48acc6cc6382520
KeceK <jfk.abrao@hotmail.com>
2025-02-18 16:10:14 -0300

    v43

---
+++ Assets/Scripts/Core/Player/TankPlayer.cs
@@ -0,0 +1,11 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class TankPlayer : NetworkBehaviour
+{
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+    }
+}
