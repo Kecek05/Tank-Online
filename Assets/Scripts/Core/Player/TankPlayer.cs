b3e0d6d0d0b011a52774c92ff54752e448a37f13
João Felipe <jfk.abrao@hotmail.com>
2025-02-19 13:39:38 -0300

    v45
    Camera bounds

--- Assets/Scripts/Core/Player/TankPlayer.cs
+++ Assets/Scripts/Core/Player/TankPlayer.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.Cinemachine;
 using Unity.Netcode;
 using UnityEngine;
@@ -7,6 +8,7 @@ public class TankPlayer : NetworkBehaviour
 
     [Header("References ")]
     [SerializeField] private CinemachineCamera playerCam;
+    [SerializeField] private CinemachineConfiner2D confiner2D;
 
     [Header("Settings")]
     private int ownerPriority = 10;
@@ -16,6 +18,21 @@ public class TankPlayer : NetworkBehaviour
         if (IsOwner)
         {
             playerCam.Priority.Value = ownerPriority;
+            StartCoroutine(DelayCamerabounds());
+        }
+    }
+
+    private IEnumerator DelayCamerabounds()
+    {
+        //Delay to set the camera bounds
+
+        while (confiner2D.BoundingShape2D == null)
+        {
+            GameObject cameraBounds = GameObject.FindGameObjectWithTag("CameraBounds");
+            if (cameraBounds != null)
+                confiner2D.BoundingShape2D = cameraBounds.GetComponent<PolygonCollider2D>();
+                
+            yield return null;
         }
     }
 }
