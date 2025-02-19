0e41980102caaba8655b5b2fabf362ef39d09c21
João Felipe <jfk.abrao@hotmail.com>
2025-02-19 12:49:25 -0300

    v44
    41. Player Camera finished

--- Assets/Scripts/Core/Player/TankPlayer.cs
+++ Assets/Scripts/Core/Player/TankPlayer.cs
@@ -1,11 +1,21 @@
+using Unity.Cinemachine;
 using Unity.Netcode;
 using UnityEngine;
 
 public class TankPlayer : NetworkBehaviour
 {
 
+    [Header("References ")]
+    [SerializeField] private CinemachineCamera playerCam;
+
+    [Header("Settings")]
+    private int ownerPriority = 10;
+
     public override void OnNetworkSpawn()
     {
-        base.OnNetworkSpawn();
+        if (IsOwner)
+        {
+            playerCam.Priority.Value = ownerPriority;
+        }
     }
 }
