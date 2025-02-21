57a9c5b055f0dd839973a05a3b75c04b4086d0d4
KeceK <jfk.abrao@hotmail.com>
2025-02-21 10:42:58 -0300

    v58
    Start Bounty Coin

--- Assets/Scripts/Core/Coins/CoinWallet.cs
+++ Assets/Scripts/Core/Coins/CoinWallet.cs
@@ -1,9 +1,45 @@
+using System;
 using Unity.Netcode;
 using UnityEngine;
 
 public class CoinWallet : NetworkBehaviour
 {
+    [Header("References")]
+    [SerializeField] private Health health;
+
+    [SerializeField] private BountyCoin bountyCoinPrefab;
+
+    [Header("Settings")]
+    [SerializeField] private float coinSpread = 3f;
+    [SerializeField] private int bountyCoinCount = 20;
+    [SerializeField] private int minBountyCoinValue = 5;
+    [SerializeField] private LayerMask layerMask;
+
+    private Collider2D[] coinBuffer = new Collider2D[1];
+
+    private float coinRadius;
     public NetworkVariable<int> totalCoins { get; private set; } = new();
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsServer) return;
+
+        coinRadius = bountyCoinPrefab.GetComponent<CircleCollider2D>().radius;
+
+        health.OnDie += HandleDeath;
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (!IsServer) return;
+
+        health.OnDie -= HandleDeath;
+    }
+
+    private void HandleDeath(Health health)
+    {
+        throw new NotImplementedException();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
